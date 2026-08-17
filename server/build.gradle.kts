0ba51d861d11e9836632228a25eebcd52906d8d7
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2026-08-17 13:53:20 +0100

    v1.0.6

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -48,7 +48,7 @@ sourceSets {
 }
 
 // should be bumped with each stable release
-val m_extension_serverVersion = "v1.0.5"
+val m_extension_serverVersion = "v1.0.6"
 
 // counts commit count on master
 val m_extension_serverRevision = runCatching {
